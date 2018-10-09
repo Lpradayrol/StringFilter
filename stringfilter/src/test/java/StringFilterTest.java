66e1f886a8282007a74a465109bdec088778adec
Lpradayrol <lpradayrol.etu@gmail.com>
2018-10-09 11:34:51 +0200

    a

--- stringfilter/src/test/java/StringFilterTest.java
+++ stringfilter/src/test/java/StringFilterTest.java
@@ -44,4 +44,55 @@ public class StringFilterTest {
 
         assertThat(output, equalTo("aire"));
     }
+
+    @Test
+    public void Upper_Post_Test(){
+
+        String input = "cybersecurite";
+        StringFilter filter1 = new PostFixStringFilter(8);
+        String output1 = filter1.filter(input);
+        assertThat(output1, equalTo("securite"));
+
+        StringFilter filter2 = new UpperCaseStringFilter();
+        String output2 = filter2.filter(output1);
+        assertThat(output2, equalTo("SECURITE"));
+    }
+
+    @Test
+    public void Upper_Post_Pre_Test(){
+
+        String input = "cybersecurite";
+        StringFilter filter1 = new PostFixStringFilter(8);
+        String output1 = filter1.filter(input);
+        assertThat(output1, equalTo("securite"));
+
+        StringFilter filter2 = new UpperCaseStringFilter();
+        String output2 = filter2.filter(output1);
+        assertThat(output2, equalTo("SECURITE"));
+
+        StringFilter filter3 = new PrefixStringFilter(4);
+        String output3 = filter3.filter(output2);
+        assertThat(output3, equalTo("SECU"));
+    }
+
+    @Test
+    public void Upper_Post_Pre_Lower_Test(){
+
+        String input = "cybersecurite";
+        StringFilter filter1 = new PostFixStringFilter(8);
+        String output1 = filter1.filter(input);
+        assertThat(output1, equalTo("securite"));
+
+        StringFilter filter2 = new UpperCaseStringFilter();
+        String output2 = filter2.filter(output1);
+        assertThat(output2, equalTo("SECURITE"));
+
+        StringFilter filter3 = new PrefixStringFilter(4);
+        String output3 = filter3.filter(output2);
+        assertThat(output3, equalTo("SECU"));
+
+        StringFilter filter4 = new LowerCaseStringFilter();
+        String output4 = filter4.filter(output3);
+        assertThat(output4, equalTo("secu"));
+    }
 }
