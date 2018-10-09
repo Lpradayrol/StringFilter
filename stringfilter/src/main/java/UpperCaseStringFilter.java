1db0ba89c10fa5b92380007bdae629f8f9c6f1d1
Lpradayrol <lpradayrol.etu@gmail.com>
2018-10-09 11:01:28 +0200

    a

--- stringfilter/src/main/java/UpperCaseStringFilter.java
+++ stringfilter/src/main/java/UpperCaseStringFilter.java
@@ -2,7 +2,9 @@ public class UpperCaseStringFilter implements StringFilter {
 
     @Override
     public String filter(String string) {
+
         //System.out.println(string.toUpperCase());
         return string.toUpperCase();
+
     }
 }
